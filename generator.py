662b87ed9f20f23a0bb8f323952b92a098d90330
Jesse Mather <jmather@arista.com>
2019-10-22 13:59:51 -0700

    generate script and configs

---
+++ generator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import jinja2
+import yaml
+
+conf = None
+with open("config.yml", "r") as fh:
+    conf = yaml.load(fh.read())
+
+print(conf)
+
+def write_file(contents, file):
+    with open(file, "w") as fh:
+        fh.write(contents)
+
+with open("reimage.py.j2", "r") as fh:
+    tpl = jinja2.Template(fh.read())
+    out = tpl.render(conf)
+    write_file(out, "reimage.py")
+
+
+with open("startup-config.j2", "r") as fh:
+    tpl = jinja2.Template(fh.read())
+    out = tpl.render(conf)
+    write_file(out, "startup-config")
+
+with open("dhcpd.conf.j2", "r") as fh:
+    tpl = jinja2.Template(fh.read())
+    out = tpl.render(conf)
+    write_file(out, "dhcpd.conf")
+
+
